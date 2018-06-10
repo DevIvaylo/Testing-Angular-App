ab262daaef409a50d9175ee441913470e87776da
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-06-10 10:57:30 +0100

    simple inline template for testing purposes

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  template: `
+    <h2 style="background-color: skyblue">About</h2>
+    <h3 style="background-color: aqua">This is about page</h3>
+  `,
+})
+export class AboutComponent {
+
+
+}
